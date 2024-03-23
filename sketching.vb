701efa0cea0a92a948a0f3588a12b9d122e1944a
hayesall <hayesall@iu.edu>
2024-03-23 13:19:21 -0400

    ♻️ Rename `color` -> `Color`

--- sketching.vb
+++ sketching.vb
@@ -3,16 +3,16 @@ for xcoord = -4.5 to 7.4 step (1/120)
   { z = complex(xcoord, ycoord)
     for iter = 1 to 12
     {  if (abs(z) < 1.1 and abs(z) > 0.9)
-      {  if (iter == 1)       color = gold
-         else if (iter < 3)   color = yellow
-         else if (iter < 5)   color = green
-         else if (iter < 7)   color = blue
-         else if (iter < 9)   color = purple
-         else if (iter < 11)  color = magenta
-         else color = red
+      {  if (iter == 1)       Color = gold
+         else if (iter < 3)   Color = yellow
+         else if (iter < 5)   Color = green
+         else if (iter < 7)   Color = blue
+         else if (iter < 9)   Color = purple
+         else if (iter < 11)  Color = magenta
+         else                 Color = red
          ' go to Plot
         }
     z = ((1-i)*z^4+(7+i)*z)/(2*z^3+6)}
-  color = black
+  Color = black
   }
 }
